1f4c4df080b5da9aae54a1345541a94b17cfd85b
AlexTing15 <163134553+AlexTing15@users.noreply.github.com>
2025-10-21 10:38:07 +0800

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace Lab2;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+
+        window.Width = 400;
+
+        window.Height = 850;
+
+        return window;
+    }
+}
